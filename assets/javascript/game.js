de62e2ccac606df8c42a81e7ea9b961ec62b0bd6
darkzero_omega <darkzero_omega2003@yahoo.com>
2018-02-24 12:41:00 -0500

    added frames fro inf stones, and updated js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,52 @@
-//  Gem Buttons//
+//-- Speudo Coding --//
+//-- A game with 4 crytals and a random result --//
+//-- every gem neeeds to have a random number between 1 and 15 --//
+//-- a new random number should generate every time we win or lose --//
+//-- when clickng any crystal, it should be adding with the previous result --//
+//-- Untilit equals the Random Result --//
+//-- if it is greater than the Random Reuslt, the weincrement a loss --//
+//-- if it IS equal, then we increment a win counter--//
 
-// Gem Amount //
 
-// Amount Randomizer //
 
-// Goal Randomizer //
 
-// Game Start //
+var random_result;
+var lost;
+var win;
 
-// Game Win //
+random_result = Math.floor(Math.random() * 90) + 30; // -- hoisting --//
 
-// Game Lost //
+console.log(random_result);
 
-// Score Total //
+$("#result").html("Random Result: " + random_result);
 
+/for(var i = 0; i < 4; i++){
+
+    var random = Math.floor(Math.random() * 15) + 1 ;
+    console.log(random);
+    
+    var infStones = $("<div>");
+        infStones.attr({
+            "class": "infStones",
+            "data-random": random
+    
+    });
+
+    $(".crystals").append(infStones);
+    
+}
+
+$(".increaseScore").each(function() {
+    var random = Math.floor(Math.random() * 15) + 1;
+    $(this).attr("data-random", random);
+})
+
+$(".increaseScore").on("click", function () {
+
+    console.log($(this).attr("data-random"));
+
+
+});
 
 
 
